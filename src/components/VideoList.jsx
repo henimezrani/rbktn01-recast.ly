13b408c388c69262fbb786ba7e6c79b3b96dc3c5
henimezrani <henimezrani@gmail.com>
2019-12-10 15:48:29 +0100

    list done

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,17 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
-);
+import exampleVideoData from '../data/exampleVideoData.js';
+import VideoListEntry from './VideoListEntry.js';
+
+var VideoList = (props) =>(
+
+    <div className="video-list">
+
+    {props.videos.map( (item) =>(
+
+      <VideoListEntry item = {item}/>
+    ))}
+
+    </div> )
+
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
@@ -14,6 +19,12 @@ VideoList.propTypes = {
   videos: React.PropTypes.array.isRequired
 };
 
+
+
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 export default VideoList;
+
+
+
+
